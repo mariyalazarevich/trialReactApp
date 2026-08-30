73c942d3b3a39fbf939abcfa35b83a99c309d469
marialazarevic <lazarevichmaryia2804@gmail.com>
2026-08-30 21:27:31 +0300

    fixed styles

--- src/components/starComponent/StarComponent.tsx
+++ src/components/starComponent/StarComponent.tsx
@@ -1,11 +1,22 @@
 import StarIcon from '../../assets/star.svg';
+import styles from './starComponent.module.css';
 
 export const StarComponent = () => (
-  <div>
-    <StarIcon />
-    <StarIcon />
-    <StarIcon />
-    <StarIcon />
-    <StarIcon />
+  <div className={styles.stars}>
+    <div className={styles.star}>
+      <StarIcon />
+    </div>
+    <div className={styles.star}>
+      <StarIcon />
+    </div>
+    <div className={styles.star}>
+      <StarIcon />
+    </div>
+    <div className={styles.star}>
+      <StarIcon />
+    </div>
+    <div className={styles.star}>
+      <StarIcon />
+    </div>
   </div>
 );
